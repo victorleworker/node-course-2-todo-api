d098acad0e317f57ee6381e4442cc1892af47a75
le hoang viet <viet.le.worker@gmail.com>
2018-08-07 14:15:30 +0700

    first commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,43 @@
+﻿//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+
+var obj = new ObjectID();
+console.log(obj);
+
+
+
+MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    /*
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    }, (eer, result) => {
+        if (err) {
+            return console.log('Unable to insert to do', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });*/
+
+    //insert new doc into Users (name,age,locaton)
+    /*
+    db.collection('Users').insertOne({
+        name: 'Andrew',
+        age: 25,
+        location: 'Philadelphia'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert user', err);
+        }
+        console.log(result.ops);
+    });
+    */
+    client.close();
+
+});
